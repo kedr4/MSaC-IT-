6c657c36ef6809cb0697e4b1f6d3d6c60a6f8cf0
Onegiiin <115876035+Onegiiin@users.noreply.github.com>
2024-09-15 21:07:20 +0300

    Add files via upload

---
+++ lab1/HalsteadCalculator/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace HalsteadCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.srcBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tBoxResult = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // srcBtn
+            // 
+            this.srcBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.srcBtn.Location = new System.Drawing.Point(133, 46);
+            this.srcBtn.Name = "srcBtn";
+            this.srcBtn.Size = new System.Drawing.Size(330, 37);
+            this.srcBtn.TabIndex = 11;
+            this.srcBtn.Text = "Выбрать файл";
+            this.srcBtn.UseVisualStyleBackColor = true;
+            this.srcBtn.Click += new System.EventHandler(this.srcBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startBtn.Location = new System.Drawing.Point(133, 118);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(330, 37);
+            this.startBtn.TabIndex = 12;
+            this.startBtn.Text = "Анализировать";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(181, 383);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // tBoxResult
+            // 
+            this.tBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBoxResult.Location = new System.Drawing.Point(133, 183);
+            this.tBoxResult.Name = "tBoxResult";
+            this.tBoxResult.ReadOnly = true;
+            this.tBoxResult.Size = new System.Drawing.Size(330, 180);
+            this.tBoxResult.TabIndex = 14;
+            this.tBoxResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 531);
+            this.Controls.Add(this.tBoxResult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.srcBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button srcBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox tBoxResult;
+    }
+}
+
---
+++ lab1/HalsteadCalculator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace HalsteadCalculator
+{
+    public partial class Form1 : Form
+    {
+        public string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void srcBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Scala Files|*.scala";
+            openFileDialog.Title = "Выберите файл";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                startBtn.Enabled = true;
+                filePath = openFileDialog.FileName;
+            }
+        }
+
+        public void AnalyzeFile(string filePath)
+        {
+      
+                string[] lines = File.ReadAllLines(filePath);
+
+                HashSet<string> uniqueOperators = new HashSet<string>();
+                HashSet<string> uniqueOperands = new HashSet<string>();
+                int totalOperatorCount = 0;
+                int totalOperandCount = 0;
+
+                foreach (string line in lines)
+                {
+
+                MatchCollection operatorMatches = Regex.Matches(line,
+         @"(\+|\-|\*|\/|\%|\=\=|\!\=|\>|\<|\>=|\<=|\&&|\|\||!|::|\<\-|\>\>|\<\<|\+=|\-=|\*=|/=|\&|\||\^|\~|\=\>|\:\:|\#|\-\>)");
+                foreach (Match match in operatorMatches)
+                    {
+                        uniqueOperators.Add(match.Value); 
+                    }
+                    totalOperatorCount += operatorMatches.Count;
+
+                MatchCollection operandMatches = Regex.Matches(line, @"([a-zA-Z_\$][a-zA-Z0-9_\$]*)");
+                foreach (Match match in operandMatches)
+                    {
+                        uniqueOperands.Add(match.Value); 
+                    }
+                    totalOperandCount += operandMatches.Count;
+                }
+
+                int uniqueOperatorCount = uniqueOperators.Count;
+                int uniqueOperandCount = uniqueOperands.Count;
+
+                tBoxResult.Text = $"Unique Operators: {uniqueOperatorCount}\n" +
+                                  $"Unique Operands: {uniqueOperandCount}\n" +
+                                  $"Total Operators: {totalOperatorCount}\n" +
+                                  $"Total Operands: {totalOperandCount}\n" +
+                                  $"Program Length: {totalOperatorCount + totalOperandCount}\n" +
+                                  $"Program Vocabulary: {uniqueOperatorCount + uniqueOperandCount}\n" +
+                                  $"Program Volume: {(totalOperatorCount + totalOperandCount) * (Math.Log(uniqueOperatorCount + uniqueOperandCount) / Math.Log(2))}\n";
+
+        }
+
+        public void startBtn_Click(object sender, EventArgs e)
+        {
+            var fileName = filePath;
+            AnalyzeFile(fileName);
+        }
+    }
+}
