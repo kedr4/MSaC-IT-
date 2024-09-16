26f94cf49f9f969982a25854f79f2e65c40173ce
kedr4 <85400754+kedr4@users.noreply.github.com>
2024-09-16 18:15:57 +0300

    added support for methods

--- lab1/HalsteadCalculator/AnalyzeInfo.cs
+++ lab1/HalsteadCalculator/AnalyzeInfo.cs
@@ -17,7 +17,17 @@ namespace HalsteadCalculator
         // Зарезервированные слова и типы для фильтрации
         private static readonly HashSet<string> ReservedWords = new HashSet<string>
         {
-            "object", "def", "val", "Unit", "String", "Int", "Array" // Добавьте сюда другие зарезервированные слова и типы
+            "abstract", "case", "catch", "class", "def", "do", "else", "extends",
+            "false", "final", "finally", "for", "forSome", "if", "implicit",
+            "import", "lazy", "match", "new", "null", "object", "override",
+            "package", "private", "protected", "return", "sealed", "super",
+            "this", "throw", "trait", "try", "true", "type", "val", "var",
+            "while", "with", "yield",
+
+            "Any", "AnyRef", "AnyVal", "Boolean", "Byte", "Char", "Double",
+            "Float", "Int", "Long", "Short", "String", "Unit", "Nothing",
+            "Null", "Option", "Some", "None", "Either", "Left", "Right",
+            "Seq", "List", "Array", "Map", "Set", "Tuple"
         };
 
         public void AnalyzeFile(string filePath)
@@ -28,6 +38,7 @@ namespace HalsteadCalculator
             OperandCount = new Dictionary<string, int>();
 
             var functionCalls = new HashSet<string>();
+            var methodsCalled = new HashSet<string>();
 
             foreach (string line in lines)
             {
@@ -51,7 +62,7 @@ namespace HalsteadCalculator
 
                 // Регулярное выражение для операторов
                 MatchCollection operatorMatches = Regex.Matches(line,
-          @"(\+=|\-=|\*=|/=|==|\!=|\>=|\<=|\->|\=>|\|\||&&|::|<<|>>|\+|\-|\*|\/|\%|\=|\!|\>|\<|\&|\||\^|\~|\#)");
+                  @"(\+=|\-=|\*=|/=|==|\!=|\>=|\<=|\->|\=>|\|\||&&|::|<<|>>|\+|\-|\*|\/|\%|\=|\!|\>|\<|\&|\||\^|\~|\#)");
 
                 foreach (Match match in operatorMatches)
                 {
@@ -66,15 +77,34 @@ namespace HalsteadCalculator
                     }
                 }
 
+                // Регулярное выражение для методов, вызванных через точку (например, .toInt)
+                MatchCollection methodCallMatches = Regex.Matches(line, @"\.[a-zA-Z_\$][a-zA-Z0-9_\$]*\b");
+
+                foreach (Match match in methodCallMatches)
+                {
+                    string methodCall = match.Value.TrimStart('.');
+                    methodsCalled.Add(methodCall);  // Добавляем метод в список вызванных методов
+
+                    if (OperatorCount.ContainsKey(methodCall))
+                    {
+                        OperatorCount[methodCall]++;
+                    }
+                    else
+                    {
+                        OperatorCount[methodCall] = 1;
+                    }
+                }
+
                 // Регулярное выражение для операндов
                 MatchCollection operandMatches = Regex.Matches(line, @"\b[a-zA-Z_\$][a-zA-Z0-9_\$]*\b");
 
                 foreach (Match match in operandMatches)
                 {
                     string operand = match.Value;
-                    // Проверяем, что это не функция или зарезервированное слово
+                    // Проверяем, что это не функция, не зарезервированное слово и не метод, вызванный через точку
                     if (!functionCalls.Contains(operand) &&
                         !ReservedWords.Contains(operand) &&
+                        !methodsCalled.Contains(operand) && // Не добавляем методы, вызванные через точку, как операнды
                         !IsOutputLine(line)) // Проверяем, что это не строка вывода
                     {
                         if (OperandCount.ContainsKey(operand))
@@ -98,6 +128,7 @@ namespace HalsteadCalculator
             ProgramVocabulary = uniqueOperatorCount + uniqueOperandCount;
             ProgramVolume = ProgramLength * (Math.Log(ProgramVocabulary) / Math.Log(2));
         }
+
 
         // Метод для определения строк вывода
         private bool IsOutputLine(string line)
